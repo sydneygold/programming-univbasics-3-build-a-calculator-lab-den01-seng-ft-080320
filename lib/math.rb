d5fbadfa83475fff956e49bcc92c6ca74e708cec
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-16 17:45:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,19 +5,27 @@ sum
 end
 
 def subtraction(num1, num2)
-
+sum = num1 - num2
+puts sum
+sum
 end
 
 def division(num1, num2)
-
+sum = num1 / num2
+puts sum
+sum
 end
 
 def multiplication(num1, num2)
-
+sum = num1 * num2
+puts sum
+sum
 end
 
 def modulo(num1, num2)
-
+result = num1 % num2
+puts result
+result
 end
 
 def square_root(num)
